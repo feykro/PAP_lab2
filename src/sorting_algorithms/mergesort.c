625f9ddf4afeb609f0067113be603c7183cd0c62
feykro <feykrotv@gmail.com>
2021-03-12 17:11:32 +0100

    debugged odd-even and merge, added gitignore

--- src/sorting_algorithms/mergesort.c
+++ src/sorting_algorithms/mergesort.c
@@ -15,51 +15,38 @@
    The two chunks are of size size
    First chunck starts at T[0], second chunck starts at T[size]
 */
-void merge (uint64_t *T, const uint64_t size)
-{
-  uint64_t *X = (uint64_t *) malloc (2 * size * sizeof(uint64_t)) ;
+void merge (uint64_t *T, const uint64_t size){
+	uint64_t *X = (uint64_t *) malloc (2 * size * sizeof(uint64_t)) ;
+	
+	uint64_t i = 0 ;
+	uint64_t j = size ;
+	uint64_t k = 0 ;
   
-  uint64_t i = 0 ;
-  uint64_t j = size ;
-  uint64_t k = 0 ;
-  
-  while ((i < size) && (j < 2*size))
-    {
-      if (T[i] < T [j])
-	{
-	  X [k] = T [i] ;
-	  i = i + 1 ;
+	while ((i < size) && (j < 2*size)){
+		if (T[i] < T [j]){
+			X [k] = T [i] ;
+			i = i + 1 ;
+		}else{
+			X [k] = T [j] ;
+			j = j + 1 ;
+		}
+		k = k + 1 ;
 	}
-      else
-	{
-	  X [k] = T [j] ;
-	  j = j + 1 ;
-	}
-      k = k + 1 ;
-    }
 
-  if (i < size)
-    {
-      for (; i < size; i++, k++)
-	{
-	  X [k] = T [i] ;
+	if (i < size){
+		for (; i < size; i++, k++){
+			X [k] = T [i] ;
+		}
+	}else{
+		for (; j < 2*size; j++, k++){
+			X [k] = T [j] ;
+		}
 	}
-    }
-  else
-    {
-      for (; j < 2*size; j++, k++)
-	{
-	  X [k] = T [j] ;
-	}
-    }
-  
-  memcpy (T, X, 2*size*sizeof(uint64_t)) ;
-  free (X) ;
-  
-  return ;
+	memcpy (T, X, 2*size*sizeof(uint64_t)) ;
+	free (X) ;
+	
+	return ;
 }
-
-
 
 
 
@@ -114,7 +101,6 @@ void parallel_merge_sort_V2 (uint64_t *T, const uint64_t size)
 
 void parallel_merge_sort (uint64_t *T, const uint64_t size)
 {
-  omp_set_num_threads(MAX_THREADS);
   #pragma omp parallel 
   {
     if (PARALLEL_V == 1){
@@ -126,6 +112,8 @@ void parallel_merge_sort (uint64_t *T, const uint64_t size)
     }
   }
 }
+
+
 
 int main (int argc, char **argv)
 {
@@ -230,7 +218,6 @@ int main (int argc, char **argv)
     /* before terminating, we run one extra test of the algorithm */
     uint64_t *Y = (uint64_t *) malloc (N * sizeof(uint64_t)) ;
     uint64_t *Z = (uint64_t *) malloc (N * sizeof(uint64_t)) ;
-    uint64_t *K = (uint64_t *) malloc (N * sizeof(uint64_t)) ;
 
 #ifdef RINIT
     init_array_random (Y, N);
@@ -239,13 +226,11 @@ int main (int argc, char **argv)
 #endif
 
     memcpy(Z, Y, N * sizeof(uint64_t));
-    memcpy(K, Y, N * sizeof(uint64_t));
 
     sequential_merge_sort (Y, N) ;
-
     parallel_merge_sort (Z, N) ;
 
-    if (! (are_vector_equals (Y, Z, N) && are_vector_equals (Y, K, N)) ) {
+    if (! are_vector_equals (Y, Z, N)) {
         fprintf(stderr, "ERROR: sorting with the sequential and the parallel algorithm does not give the same result\n") ;
         exit (-1) ;
     }
