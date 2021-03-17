6b7fb81f4cc22a3be672c09dce7fa409896d3e8c
feykro <feykrotv@gmail.com>
2021-03-17 11:29:18 +0100

    finished quick sort para

--- src/sorting_algorithms/quick-sort.c
+++ src/sorting_algorithms/quick-sort.c
@@ -18,6 +18,22 @@ void swap(uint64_t *a, uint64_t *b) {
 }
 
 
+
+/**
+ * Du coup on s'en sert pas vu qu'on a implémenté notre version
+ */
+/*
+int comparInt(const void * a, const void * b){
+    uint64_t inta = *((uint64_t *) a);
+    uint64_t intb = *((uint64_t *) b);
+    if(a == b){
+        return 0;
+    }else{
+        return a < b ? -1 : 1;
+    }
+}
+*/
+
 int partition(uint64_t * T, int first, int last) {
     int pivot = T[last];
     int i = (first - 1);
@@ -41,6 +57,45 @@ void quickSort(uint64_t * T, int first, int last) {
     }
 }
 
+//===========================================
+
+int omegaPartition(uint64_t indices[], uint64_t entiers[], int first, int last) {
+    int pivot = entiers[last];
+    int i = (first - 1);
+
+    for (int j=first; j < last; j++) {
+        if (entiers[j] <= pivot) {
+        i++;
+        swap(&entiers[i], &entiers[j]);
+        swap(&indices[i], &indices[j]);
+        }
+    }
+
+    swap(&entiers[i+1], &entiers[last]);
+    swap(&indices[i+1], &indices[last]);
+    return (i+1);
+}
+
+void omegaQuickSort(uint64_t indices[], uint64_t entiers[], int first, int last) {
+    if (first < last) {
+        int pivot = omegaPartition(indices, entiers, first, last);
+        omegaQuickSort(indices, entiers, first, pivot - 1);
+        omegaQuickSort(indices, entiers, pivot + 1, last);
+    }
+}
+
+uint64_t * quickSortChunks(uint64_t * chunksList[], int nbChunks){
+    uint64_t * indice_list = (uint64_t *) malloc(sizeof(uint64_t)* nbChunks);
+    uint64_t first_elem_list[nbChunks];
+    for(int i=0; i<nbChunks; i++){
+        indice_list[i] = i;
+        first_elem_list[i] = chunksList[i][0];
+    }
+    omegaQuickSort(indice_list, first_elem_list, 0, nbChunks-1);
+    return indice_list;
+}
+
+//================================================
 
 void sequential_quick_sort (uint64_t *T, int size){
     quickSort(T, 0, size-1);
@@ -48,12 +103,67 @@ void sequential_quick_sort (uint64_t *T, int size){
 }
 
 void parallel_quick_sort (uint64_t *T, int size){
-    //actuellement c'est de la merde mais apparemment il faut
-    //écrire une fonction de partionnement et faire deux appels
-    //a quicksort avec pragma omp task
-    omp_set_num_threads(MAX_THREADS);
-    #pragma omp task shared(T)
-    quickSort(T, 0, size-1);
+    int nbChunks = MAX_THREADS < size ? MAX_THREADS : size;
+    uint64_t chunkSize = size / nbChunks;
+    uint64_t lastChunkSize = chunkSize + size%nbChunks;
+
+    uint64_t * chunksList[nbChunks];
+    for(int i=0; i<nbChunks; i++){
+        chunksList[i] = &T[i*chunkSize];
+    }
+
+    int condition = 1;
+    int ind_chunk;
+    int nb = 0;
+    uint64_t * resultat = (uint64_t *) malloc(size * sizeof(uint64_t));
+    omp_set_num_threads(nbChunks);  //better performance with this line
+
+    #pragma omp parallel default (none) shared(nbChunks, chunksList, chunkSize, lastChunkSize, nb, resultat) private(ind_chunk, size)
+    {
+        //here we're gonna apply one bubble to every chunk
+        #pragma omp for 
+        for(ind_chunk = 0; ind_chunk < nbChunks; ind_chunk++){
+            int taille = ind_chunk == nbChunks - 1 ? lastChunkSize : chunkSize;
+            quickSort(chunksList[ind_chunk], 0, taille -1);
+        }
+
+        
+        
+        //so at this point the chunks are all sorted. What we're gonna do is make an array of the
+        //first elements of each array and quicksort it along side an array of index to keep track 
+        //of the change. Then using the index, we assemble the chunks in the right order and we win
+        uint64_t * indices = quickSortChunks(chunksList, nbChunks);
+
+        //Maintenant on reconstruit l'array résultat
+
+        #pragma omp for 
+        for(ind_chunk = nbChunks -1; ind_chunk > -1; ind_chunk--){
+            int indice = indices[ind_chunk];
+            int taille = ind_chunk == nbChunks - 1 ? lastChunkSize : chunkSize;
+            uint64_t * emplacement = resultat;
+
+            if(indice <= indices[nbChunks - 1]){
+                emplacement += indice * chunkSize;
+            }else{
+                emplacement += (indice-1) * chunkSize + lastChunkSize;
+            }
+            memcpy(emplacement , chunksList[ind_chunk], taille * sizeof(uint64_t));
+        }
+        
+    }
+    //*T = *resultat;
+    memcpy(T, resultat, size * sizeof(uint64_t));
+    /*
+    for(int i = 0; i<size; i++){
+        printf("%d ", T[i]);
+    }
+    printf("||\n");
+    int test = is_sorted(T, size);
+    printf("test : %d\n", test);
+    uint64_t control1[3] = {1, 2, 3};
+    uint64_t control2[3] = {3, 2, 1};
+    printf("Control positif : %d | Control negatif : %d |\n", is_sorted(control1, 3), is_sorted(control2, 3));
+    */
 
     return;
 }
